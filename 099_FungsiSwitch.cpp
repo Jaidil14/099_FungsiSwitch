cd04151ffa48c54331907fdc2a51cc7d71c213b5
Jaidil14 <jaidilrachmad@gmail.com>
2024-03-25 14:55:41 +0700

    Menambahkan prosedur input

--- 099_FungsiSwitch.cpp
+++ 099_FungsiSwitch.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int bilSatu, bilDua;
+
+void input ()
+{
+    cout << " Masukkan bilangan pertama : " << endl;
+    cin >> bilSatu;
+    cout << " Masukkan bilangan kedua : n" << endl;
+    cin >> bilDua;
+}
