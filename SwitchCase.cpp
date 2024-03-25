fb73c7ddcde854fed96510fc37df627f0f986d3b
Jaidil14 <jaidilrachmad@gmail.com>
2024-03-25 14:22:08 +0700

    initial commit

---
+++ SwitchCase.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input ()
+{
+    cout << " Masukkan bilangan pertama : " << endl;
+    cin >> bilSatu;
+    cout << " Masukkan bilangan kedua : n" << endl;
+    cin >> bilDua;
+}
+
+float penjumlahan (float a, float b)
+{
+    return a + b;
+}
+
+float pembagian (float a, float b)
+{
+    return a / b;
+}
+
+float perkalian (float a, float b)
+{
+    return a * b;
+}
+
+float pengurangan (float a, float b)
+{
+    return a - b;
+}
